c0cf9486da885327577a03de4e47f031faa87e32
Ibrahim Kolabalogun <115741084+bolajeee@users.noreply.github.com>
2023-06-12 18:27:17 +0100

    edit

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 let navList = document.getElementById('nav-list');
+let navList2 = document.getElementById("nav-list2");
 let burger = document.getElementById('burger');
 
-burger.addEventListener('click',() => {
-    console.log('working')
-    burger.classList.toggle('ri-close-line')
-    burger.classList.remove('bx bx-menu')
 
-    if(burger.classList.contains('bx-menu')){
-        navList.classList.add('open')
-    }else{
-        navList.classList.remove('open')
-    }
+
+
+burger.addEventListener('click',() => {
+    burger.classList.toggle('active');
+    navList.classList.toggle("active");
+    navList2.classList.toggle('open');
+    
+
+if(navList2.classList.contains('open')){
+    navList2.classList.remove('nav-list2')
+}else{
+    navList2.classList.add('nav-list2')
+}
+
 })
+
